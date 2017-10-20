2864bae31e6bc77f50e847fc1aa137fbcb1ed33b
Markus Wind <markuswind@gmail.com>
2017-10-20 19:14:03 +0200

    added base for retrieving/saving recent searches

--- Flickr-MVVM/ViewModels/SearchViewModel.swift
+++ Flickr-MVVM/ViewModels/SearchViewModel.swift
@@ -6,8 +6,31 @@
 //  Copyright © 2017 Markus Wind. All rights reserved.
 //
 
+import Foundation
+
 class SearchViewModel {
 
-    let reuseIdentifier = "PhotoCollectionViewCell"
+    let reuseIdentifier = "SearchViewCell"
+    let searchDataKey = "SearchDataKey"
+    
+    var searchData: [String] = []
+
+    func retrieveInitialSearchData() {
+        if let data: Any = UserDefaults.standard.object(forKey: searchDataKey) {
+            searchData = data as! [String]
+        }
+    }
+
+    private func saveSearch(text: String) {
+        // FIXME: - should not be case sensitive
+        let uniqueSearchData = searchData.filter { $0 != text }
+
+        searchData = uniqueSearchData
+        searchData.insert(text, at: 0)
+
+        let defaults = UserDefaults.standard
+        defaults.set(searchData, forKey: searchDataKey)
+        defaults.synchronize()
+    }
 
 }
