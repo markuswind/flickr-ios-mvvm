ea04823ae53c076d4422bdcdf58c63b3b34baa7c
Markus Wind <markuswind@gmail.com>
2017-11-06 16:11:23 +0100

    added loading of api key/secret from configuration

--- flickr-ios-mvvm/Stores/Store.swift
+++ flickr-ios-mvvm/Stores/Store.swift
@@ -12,20 +12,30 @@ class Store {
 
   // MARK: - Properties
 
-  let baseURL = ""
+  let baseURL = "https://api.flickr.com/services/rest/?"
   let apiKey: String!
   let apiSecret: String!
 
   // MARK: - Lifecycle
 
   init() {
-    apiKey = ""
-    apiSecret = ""
+    guard let path = Bundle.main.path(forResource: "configuration", ofType: "plist") else {
+      fatalError("You probably forgot to add a configuration file..")
+    }
+
+    guard let configDictionary = NSDictionary(contentsOfFile: path) else {
+      fatalError("Something went wrong when loading configurion file..")
+    }
+
+    apiKey = configDictionary["apiKey"] as! String
+    apiSecret = configDictionary["apiSecret"] as! String
   }
 
   // MARK: - API Requests
 
   func getRequest(url: String, completion:@escaping () -> ()) {
+    let url = url + "&api_key=\(apiKey)"
+
     Alamofire.request(url, method: .get).validate().responseJSON { response in
       switch response.result {
         case .success(let value):
